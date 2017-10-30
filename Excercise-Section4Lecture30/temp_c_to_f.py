e3d9ae32ea27106116e89eefd20a09c896d5f2b7
Mark J Kirby <mark@kirbyware.com>
2017-10-30 16:34:20 -0400

    updated

--- Excercise-Section4Lecture30/temp_c_to_f.py
+++ Excercise-Section4Lecture30/temp_c_to_f.py
@@ -1,5 +1,12 @@
 def temp_c_to_f(celsius):
-    fahrenheit = celsius * 9/5 + 32
-    return fahrenheit
+    if float(celsius) < -273.15:
+        print("The lowest possible temperature that physical matter can reach is -273.15 °C")
+    else:
+        fahrenheit = celsius * 9 / 5 + 32
+        return fahrenheit
+
 
 print(temp_c_to_f(10))
+print(temp_c_to_f(-10))
+print(temp_c_to_f(-273))
+print(temp_c_to_f(-274))
