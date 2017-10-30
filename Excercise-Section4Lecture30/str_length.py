66028d48584d589b757d4d4e23bc24dabf39ef4d
Mark J Kirby <mark@kirbyware.com>
2017-10-30 16:30:34 -0400

    updated

--- Excercise-Section4Lecture30/str_length.py
+++ Excercise-Section4Lecture30/str_length.py
@@ -1,9 +1,12 @@
 def string_len(string):
     if type(string) == int:
-        return "Sorry, integers dont have length"
+        return "Sorry, integers don't have length"
+    elif type(string) == float:
+        return "Sorry, floats don't have length"
     else:
         return len(string)
 
 
 print(string_len("Hello Mark"))
 print(string_len(10))
+print(string_len(1.5))
