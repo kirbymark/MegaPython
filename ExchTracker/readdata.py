b430725ab973b16dc6ad561f7d5303220193369f
Mark J Kirby <mark@kirbyware.com>
2017-10-30 20:40:27 -0400

    working

--- ExchTracker/readdata.py
+++ ExchTracker/readdata.py
@@ -1,5 +1,5 @@
 import csv
-with open('export-EtherPrice.csv', 'r') as f:
+with open('cd-EtherPrice.csv', 'r') as f:
     reader = csv.reader(f)
     ethdata = list(reader)
 
@@ -11,16 +11,21 @@ eth_amt=0.0
 fee_percent=0.003
 target_sell=0.00
 
-for i in ethdata[150:160]:
+for i in ethdata[1:10]:
     print(i)
     date=i[0]
-    price=float(i[2])
+    price=float(i[1])
 
-    if action == "BUY":
-        if price > 0.00:
-            eth_amt = eth_amt + (usd_amt-(usd_amt*fee_percent))/price
-            target_sell = price * 1.05
-            usd_amt = 0.00
-            action="SELL"
-    print(date + " " + str(usd_amt) + " " + str(eth_amt)+ " "+ str(target_sell))
+# if action == "BUY":
+#        if price > 0.00:
+#            eth_amt = eth_amt + (usd_amt-(usd_amt*fee_percent))/price
+#            target_sell = price * 1.05
+#            usd_amt = 0.00
+#            action="SELL"
+#
+#    if action=="SELL":
+#        if price > target_sell:
+#
+#
+#    print(date + " " + str(usd_amt) + " " + str(eth_amt)+ " "+ str(target_sell))
 #    print(i[3])
