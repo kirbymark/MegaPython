c682772b5dd56c61f5988727d4c68a9164d2450c
Mark J Kirby <mark@kirbyware.com>
2017-12-05 13:44:41 -0500

    update version

--- Prg1/testOxford.py
+++ Prg1/testOxford.py
@@ -20,7 +20,7 @@ def getDictdata(word_id,language):
         return None
 
 
-response = getDictdata('rain','en')
+response = getDictdata('cloud','en')
 responsePy = response.json()
 
 
@@ -31,30 +31,8 @@ Trying  jsonpath
 jsonpath_expr = parse('results[*]..senses[*].definitions[*]')
 
 print("----------------------------")
-print("searching for :" + str(jsonpath_expr))
+print("Entries for :" + "cloud")
 print("----------------------------")
 for match in jsonpath_expr.find(responsePy):
-    print(match.full_path)
-    print(match.value)
-
-#location = parse(match.full_path)
-#    for output in location.find(responsePy):
-#        print(output)
-
-
-'''
-Trying  jsonpath
-'''
-#jsonpath_expr = parse('entries[*].senses[*].definitions')
-jsonpath_expr = parse('results.[0].lexicalEntries.[0].entries.[0].senses.[0]..examples[*]')
-
-print("----------------------------")
-print("searching for :" + str(jsonpath_expr))
-print("----------------------------")
-for match in jsonpath_expr.find(responsePy):
-    print(match.value)
-
-    #print(match.__dict__)
-#location = parse(match.full_path)
-#    for output in location.find(responsePy):
-#        print(output)
+    #print(match.full_path)
+    print("- " + match.value)
