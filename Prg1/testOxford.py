cbe7bab8f1ea51a702711aa8808a9f9899a3fa68
Mark J Kirby <mark@kirbyware.com>
2017-12-05 13:35:38 -0500

    more work on Json data

--- Prg1/testOxford.py
+++ Prg1/testOxford.py
@@ -28,11 +28,33 @@ responsePy = response.json()
 Trying  jsonpath
 '''
 #jsonpath_expr = parse('entries[*].senses[*].definitions')
-jsonpath_expr = parse('results[*]..definitions[*]')
+jsonpath_expr = parse('results[*]..senses[*].definitions[*]')
+
+print("----------------------------")
+print("searching for :" + str(jsonpath_expr))
+print("----------------------------")
+for match in jsonpath_expr.find(responsePy):
+    print(match.full_path)
+    print(match.value)
+
+#location = parse(match.full_path)
+#    for output in location.find(responsePy):
+#        print(output)
+
+
+'''
+Trying  jsonpath
+'''
+#jsonpath_expr = parse('entries[*].senses[*].definitions')
+jsonpath_expr = parse('results.[0].lexicalEntries.[0].entries.[0].senses.[0]..examples[*]')
 
 print("----------------------------")
 print("searching for :" + str(jsonpath_expr))
 print("----------------------------")
 for match in jsonpath_expr.find(responsePy):
     print(match.value)
-    print(match.full_path)
+
+    #print(match.__dict__)
+#location = parse(match.full_path)
+#    for output in location.find(responsePy):
+#        print(output)
