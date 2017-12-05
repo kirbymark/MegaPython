50551a6e48ad39fa41a34da74fc333c19fb8e6d0
Mark J Kirby <mark@kirbyware.com>
2017-12-05 10:55:24 -0500

    parsing nested json

--- Prg1/testOxford.py
+++ Prg1/testOxford.py
@@ -1,25 +1,53 @@
 import api_config
 import json
 import requests
-
-OxfordHeaders = {"app_id": api_config.oxfordDict_app_id, "app_key": api_config.oxfordDict_app_key}
-#OxfordHeaders = {"app_id": "123", "app_key": api_config.oxfordDict_app_key}
-print(OxfordHeaders)
-
-word_id = 'hello'
-language='en'
-
-url = api_config.oxfordURL +'/entries/' + language + '/' + word_id.lower() + '/regions=us'
-print(url)
-
-response = requests.get(url, headers = OxfordHeaders)
+from jsonpath_ng import jsonpath, parse
+import pandas as pd
 
 
-if response.status_code == 200:
-    print("code {}\n".format(response.status_code))
-    print("text \n" + response.text)
-    print("json \n" + json.dumps(response.json()))
-else:
-    print("Error calling the OxfordAPI")
-    print ("API Response Code: " + str(response.status_code))
-    print ("API Response Text: " + str(response.text))
+def getDictdata(word_id,language):
+    OxfordHeaders = {"app_id": api_config.oxfordDict_app_id, "app_key": api_config.oxfordDict_app_key}
+    url = api_config.oxfordURL +'/entries/' + language + '/' + word_id.lower() + '/regions=us'
+    print("Calling: " + url)
+
+    response = requests.get(url, headers = OxfordHeaders)
+
+    if response.status_code == 200:
+        print("Success")
+        return response
+    else:
+        print("Error calling the OxfordAPI")
+        print ("API Response Code: " + str(response.status_code))
+        print ("API Response Text: " + str(response.text))
+        return None
+
+
+response = getDictdata('rain','en')
+responsePy = response.json()
+output=responsePy["results"][0]["lexicalEntries"]
+#print(type(output))
+#print(len(output))
+#print(output[0])
+#print(output[1])
+print(output[1]["entries"][0]["senses"][0]["definitions"])
+print(output[0]["entries"][0]["senses"][0]["definitions"])
+
+'''
+Trying  pandas
+'''
+
+df = pd.io.json.json_normalize(responsePy)
+df.columns = df.columns.map(lambda x: x.split(".")[-1])
+print (df)
+
+
+'''
+Trying  jsonpath
+#jsonpath_expr = parse('entries[*].senses[*].definitions')
+jsonpath_expr = parse('provider[*]')
+print(jsonpath_expr)
+
+for match in jsonpath_expr.find(responsePy):
+    print(match)
+
+'''
